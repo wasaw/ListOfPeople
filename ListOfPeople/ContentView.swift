eeb5d13f685535f933caa4b5f050cc1a1bead5d5
wasaw <merenkovalexander@gmail.com>
2025-02-06 14:26:11 +0300

    init commit

---
+++ ListOfPeople/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ListOfPeople
+//
+//  Created by Александр Меренков on 06.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ListOfPeople/ListOfPeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfPeopleApp.swift
+//  ListOfPeople
+//
+//  Created by Александр Меренков on 06.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ListOfPeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
